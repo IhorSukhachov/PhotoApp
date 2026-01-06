85d776aea00f0604371711ab861090619081c710
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-06 12:18:05 +0200

    Initial Commit

---
+++ PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by Ihor Sukhachov on 06.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
