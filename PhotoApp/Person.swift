3a9445b79a1d55e7b2278b5f28c2a406d0aeaf28
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-06 12:21:21 +0200

    creating person class

--- PhotoApp/Person.swift
+++ PhotoApp/Person.swift
@@ -5,4 +5,18 @@
 //  Created by Ihor Sukhachov on 06.01.2026.
 //
 
+import SwiftData
 import Foundation
+
+@Model
+class Person: Identifiable {
+    var name: String
+    
+    @Attribute(.externalStorage)
+    var photo: Data
+
+    init(name: String, photo: Data) {
+        self.name = name
+        self.photo = photo
+    }
+}
