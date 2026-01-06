eafbe4f555e8c1a0a1faeb50115280546a348349
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-06 12:20:13 +0200

    creating necessary files

---
+++ PhotoApp/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  PhotoApp
+//
+//  Created by Ihor Sukhachov on 06.01.2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ PhotoApp/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  PhotoApp
+//
+//  Created by Ihor Sukhachov on 06.01.2026.
+//
+
+import Foundation
